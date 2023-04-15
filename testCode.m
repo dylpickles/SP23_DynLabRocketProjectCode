121ae8e3249250cf402d13d69dde1fde1cc91248
dylpickles <blorenge50@yahoo.com>
2023-04-14 22:08:07 -0400

    Some random updates

--- testCode.m
+++ testCode.m
@@ -5,7 +5,7 @@ gamma = 1.4; % adiabatic index of air
 c_d = 0.345; % estimated coefficient of drag (from NASA)
 rho_air = 1.293; % kg/m^3
 rho_water = 997; % kg/m^3
-m_rocket = 0.198; % empty rocket mass
+m_rocket = 0.198; % empty rocket mass, kg
 v_rocket = 1.25*0.001; % m^3
 r_rocket = 0.043; % m
 r_nozzle = 0.015; % m
@@ -19,7 +19,7 @@ launchPressure = input("What was the gauge launch pressure in psi? ")*psiToPa; %
 launchVolume = input("What was the initial volume of water in mL? ")*10^(-6); % converted to m^3
 initAirVolume = v_rocket - launchVolume;
 cutoffSpeed = 10; % m/s, max downward speed before ending simulation
-turbulenceFactor = 1.5; % experimental
+turbulenceFactor = 1.5; %experimental
 
 % frame 1 (initial conditions) and setting up arrays
 p = zeros(10000, 1);
@@ -40,19 +40,22 @@ i = 1; % index
 while(v(i) > -cutoffSpeed && i < 10000)
     time(i+1) = time(i) + delta_t;
     p(i+1)= p(1)*((initAirVolume + (m(1) - m(i))/rho_water)/initAirVolume)^(-gamma);
+    
+    fprintf('Frame: %i |P: %fPa|v: %fm/s|Height: %fft|m: %fkg |m_dot: %fkg/s|Time: %fs\n', i, p(i), v(i), h(i), m(i), m_dot(i), time(i));
     if(m(i) >= m_rocket)
-        disp("thrusting")
+        %disp("thrusting")
         T(i+1) = 2*pi()*r_nozzle^2*(p(i)-p_atm);
         m_dot(i+1) = -pi()*r_nozzle^2*rho_water*sqrt(2*(p(i)-p_atm)/rho_water)/turbulenceFactor;
         v(i+1) = v(i) + (T(i)/m(i) - g - (beta*v(i)*abs(v(i)))/m(i))*delta_t;
         h(i+1) = h(i) + v(i)*delta_t;
         m(i+1) = m(i) + m_dot(i)*delta_t;
     else
-        disp("weee")
+        %disp("weee")
         v(i+1) = v(i) + (-g -(beta*v(i)*abs(v(i)))/m_rocket)*delta_t;
         h(i+1) = h(i) + v(i)*delta_t;
         m(i+1) = m(i);
     end
+    
     i = i + 1;
 end
 
